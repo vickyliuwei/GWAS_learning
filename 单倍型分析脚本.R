3f6027b71f4c3ba26ac42c5f2f7318a9aeca9361
vickyliuwei <118613755+vickyliuwei@users.noreply.github.com>
2023-05-03 16:21:33 +0800

    Add files via upload

---
+++ 单倍型分析脚本.R
@@ -0,0 +1,15 @@
+geno_sub <- read.table("/w/00/g/g01/user319/test.012")
+geno_1 <- geno_sub[,-1]
+geno_1 <- geno_1[,seq(1,ncol(geno_1),10)]
+snp_1 <- pos[seq(1,nrow(pos),10),2]
+hr <- hclust(as.dist(1-cor(t(geno_1),use = "pairwise.complete.obs")), method="complete")
+mycl <- cutree(hr, k = 2)
+myCol = c("pink1", "violet")
+clusterCols <- rainbow(length(unique(mycl))) #sample(myCol,length(unique(mycl)))#
+myClusterSideBar <- clusterCols[mycl]
+heatmap.2(geno_1, main="", 
+          Rowv=as.dendrogram(hr), Colv = FALSE,scale="row", 
+          col=c("slateblue4", "olivedrab"), density.info="none", trace="none", 
+          RowSideColors= myClusterSideBar,cexCol=0.8,srtCol = 45,
+          key.title = NULL,labCol = snp_1,labRow = ind$V1)
+
